cab3bd7d5af9b08db80a735c600e34a68603f221
abdullah126 <abdullah.has126@gmail.com>
2022-08-31 16:59:25 +0500

    AddUsersController

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class UserControllerTest < ActionDispatch::IntegrationTest
+class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get user_index_url
+    get users_index_url
     assert_response :success
   end
 
