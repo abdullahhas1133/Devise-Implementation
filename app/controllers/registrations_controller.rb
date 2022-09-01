d780dba2e691cbd4d5e2effe10764fa16ed1b247
abdullah126 <abdullah.has126@gmail.com>
2022-09-01 13:12:47 +0500

    PostControllerSetup

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,29 @@
-class RegistrationsController < ApplicationController
+class RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters
+
+  def new
+    super
+  end
+
+  def create
+    super
+    # if @user.save
+    #   flash[:success] = "Admin saved!"
+    #   redirect_to :action => 'welcome'
+    # else
+    #   render action: :new
+    # end
+  end
+
+  def update
+    super
+  end
+  def welcome; end
+
+protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :encrypted_password, :status, :experience_year])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :encrypted_password, :status, :experience_year])
+  end
 end
